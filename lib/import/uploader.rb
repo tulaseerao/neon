1d9a043103ecddd2b86197b8dcec3f1c78bd01a5
tulaseerao <tulasi.chintha@gmail.com>
2013-12-25 10:44:18 -0500

    updated xlsm file type for excel uploader

--- lib/import/uploader.rb
+++ lib/import/uploader.rb
@@ -7,6 +7,7 @@ class Uploader
       when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
       when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
       when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+      when ".xlsm" then Roo::Excelx.new(file.path, nil, :ignore)
       else 
         raise "Unknown file type: #{file.original_filename}"
       end
